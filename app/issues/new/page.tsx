12b350d848b33fec3d6b6061b067ee5948466f70
Stefan Raisl <stefan.raisl@gmail.com>
2025-05-16 17:57:08 +0200

    add a markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
 import React from 'react';
-import {Button, TextArea, TextField} from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import {Button, TextField} from "@radix-ui/themes";
 
 const NewIssuePage = () => {
     return (
         <div className={"max-w-xl space-y-5"}>
             <TextField.Root placeholder={"Title"}></TextField.Root>
-            <TextArea placeholder="description" />
+            <SimpleMDE placeholder={"Description"}/>
             <Button>Submit New Issue</Button>
         </div>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import NavBar from "@/app/NavBar";
 import "@radix-ui/themes/styles.css";
-import {Theme} from "@radix-ui/themes";
+import {Theme, ThemePanel} from "@radix-ui/themes";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
         <html lang="en">
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Theme>
+        <Theme accentColor="purple">
             <NavBar/>
             <main className={'p-5'}>{children}</main>
         </Theme>
